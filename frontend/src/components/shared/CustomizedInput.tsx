26944d9dbfda7916242b3565c360f3909567061a
Hassan Kashif. pro <hassan.kashif.pro@Gmail.com>
2024-05-11 16:02:16 -0700

    Login Page GUI

---
+++ frontend/src/components/shared/CustomizedInput.tsx
@@ -0,0 +1,31 @@
+import TextField from "@mui/material/TextField";
+
+type Props = {
+  name: string;
+  type: string;
+  label: string;
+};
+
+function CustomizedInput(props: Props) {
+  return (
+    <div>
+      <TextField
+        margin="normal"
+        InputLabelProps={{ style: { color: "white" } }}
+        name={props.name}
+        type={props.type}
+        label={props.label}
+        InputProps={{
+          style: {
+            width: "400px",
+            borderRadius: 10,
+            fontSize: 20,
+            color: "white",
+          },
+        }}
+      />
+    </div>
+  );
+}
+
+export default CustomizedInput;
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 function Home() {
-  return <div></div>;
+  return <div>Hellow This is Home.</div>;
 }
 
 export default Home;
