abb31bc60deeac77f17cbf7612f2475c2d8a9c1e
Hassan Kashif. pro <hassan.kashif.pro@Gmail.com>
2024-05-20 06:18:38 -0700

    Added the "Clear Chat" <Button>

--- frontend/src/pages/Chat.tsx
+++ frontend/src/pages/Chat.tsx
@@ -1,8 +1,14 @@
-import { Avatar, Box, Typography } from "@mui/material";
+import { Avatar, Box, Button, Typography } from "@mui/material";
+import { red } from "@mui/material/colors";
 import { useAuth } from "../context/AuthContext";
 
-function Chat() {
+const Chat = () => {
   const auth = useAuth();
+  const userName = auth?.user?.name || "";
+  const initials = userName
+    ? `${userName[0]}${userName.split(" ")[1]?.[0] || ""}`
+    : "";
+
   return (
     <Box
       sx={{
@@ -14,43 +20,57 @@ function Chat() {
         gap: 3,
       }}
     >
-      <Box sx={{ display: { cd: "flex", xs: "none", sm: "none" } }}>
+      <Box sx={{ display: { md: "flex", xs: "none", sm: "none" } }}>
         <Box
           sx={{
             display: "flex",
             width: "100%",
             height: "60vh",
             bgcolor: "rgb(17,29,39)",
-            borderRadius: "5",
+            borderRadius: 5,
             flexDirection: "column",
-            mx: "3",
+            mx: 3,
           }}
         >
           <Avatar
             sx={{
               mx: "auto",
-              my: "2",
+              my: 2,
               bgcolor: "white",
               color: "black",
-              fontWeight: "700",
+              fontWeight: 700,
             }}
           >
-            {auth?.user?.name[0]}
-            {auth?.user?.name.split(" ")[1][0]}
+            {initials}
           </Avatar>
-          <Typography sx={{ mx: "auto", fontFamily: "work sans" }}>
+          <Typography sx={{ mx: "auto", fontFamily: "Work Sans" }}>
             You Are Talking To A Chat-Bot
           </Typography>
-          <Typography
-            sx={{ mx: "auto", fontFamily: "work sans", my: "4", p: 3 }}
-          >
-            You can ask anything over here which is related to Knowledege and
+          <Typography sx={{ mx: "auto", fontFamily: "Work Sans", my: 4, p: 3 }}>
+            You can ask anything over here which is related to Knowledge and
             Education...
           </Typography>
+          <Button
+            sx={{
+              width: "200",
+              my: "auto",
+              color: "white",
+              fontWeight: "700",
+              borderRadius: 3,
+              mx: "auto",
+              bgcolor: red[300],
+              ":hover": {
+                bgcolor: red.A400,
+              },
+            }}
+          >
+            Clear Conversation
+          </Button>
         </Box>
       </Box>
+      <Box sx={{ display }}></Box>
     </Box>
   );
-}
+};
 
 export default Chat;
