517e7ecbc39409bedfc4f24b573492744c83ae36
Hassan Kashif. pro <hassan.kashif.pro@Gmail.com>
2024-05-23 17:29:03 -0700

    Set Up Final Version Of Home Page.

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,7 +1,66 @@
-import React from "react";
+import { Box, useMediaQuery, useTheme } from "@mui/material";
+import TypingAnime from "../components/typer/TypingAnime";
+import Footer from "../components/footer/footer";
 
-function Home() {
-  return <div>Hellow This is Home.</div>;
-}
+const Home = () => {
+  const theme = useTheme();
+  const isBelowMd = useMediaQuery(theme.breakpoints.down("md"));
+  return (
+    <Box width={"100%"} height={"100%"}>
+      <Box
+        sx={{
+          display: "flex",
+          width: "100%",
+          flexDirection: "column",
+          alignItems: "center",
+          mx: "auto",
+          mt: 3,
+        }}
+      >
+        <Box>
+          <TypingAnime />
+        </Box>
+        <Box
+          sx={{
+            width: "100%",
+            display: "flex",
+            flexDirection: { md: "row", xs: "column", sm: "column" },
+            gap: 5,
+            my: 10,
+          }}
+        >
+          <img
+            src="robot.png"
+            alt="robot"
+            style={{ width: "200px", margin: "auto" }}
+          />
+          <img
+            className="image-inverted rotate"
+            src="openai.png"
+            alt="openai"
+            style={{ width: "200px", margin: "auto" }}
+          />
+        </Box>
+        <Box sx={{ display: "flex", mx: "auto" }}>
+          <img
+            src="chat.png"
+            alt="chatbot"
+            style={{
+              display: "flex",
+              margin: "auto",
+              width: isBelowMd ? "80%" : "60%",
+              borderRadius: 20,
+              boxShadow: "-5px -5px 105px #64f3d5",
+              marginTop: 20,
+              marginBottom: 20,
+              padding: 10,
+            }}
+          />
+        </Box>
+      </Box>
+      <Footer />
+    </Box>
+  );
+};
 
 export default Home;
